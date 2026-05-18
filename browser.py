d87481e4e7cbc80d14bfced7a62deab028fd047b
abdulkerimakten <prefront4ll0b@gmail.com>
2026-05-18 23:50:56 +0300

    Implement HTTP caching, chunked transfer decoding, and gzip compression support

--- browser.py
+++ browser.py
@@ -1,7 +1,11 @@
 import socket
 import ssl
 import os
+import time
+import gzip
+
 SOCKETS = {}
+CACHE = {}
 
 
 DEFAULT_FILE = os.path.join(os.path.dirname(__file__), "test.html")
@@ -30,7 +34,9 @@ def show(body):
 
 
 def load(url, redirects=0):
+    
     status, headers, body = url.request()
+
     if status.startswith("3"):
         if redirects >= 10:
             print("TOO MANY REDIRECTS....")
@@ -101,11 +107,36 @@ class URL:
             return "200", {}, content
         
         else:
-            key = (self.scheme, self.host, self.port)
+            socket_key = (self.scheme, self.host, self.port)
+            cache_key = (self.scheme, self.host, self.port, self.path)
+
+            if cache_key in CACHE:
+                cached_time, status, headers, body = CACHE[cache_key]
+                age = time.time() - cached_time
+
+                max_age = None
+                cache_control = headers.get("cache-control")
+                # to fetch "max_age"
+                parts = cache_control.split(",")
+                for part in parts:
+                    part = part.strip()
+                    if part.startswith("max-age="):
+                        max_age = int(part.split("=", 1)[1])
+
+                # age control
+                if max_age is not None and age < max_age:
+                    print("-------------------....LOAD FROM CACHE....-------------------")
+                    return status, headers, body
+                else:
+                    print("------------------ MAX AGE ===", max_age)
+                    print("------------------ AGE ===", age) 
+                    del CACHE[cache_key]
+                
+
 
             # Socket Manipulation
-            if key in SOCKETS:
-                s, response = SOCKETS[key]
+            if socket_key in SOCKETS:
+                s, response = SOCKETS[socket_key]
                 print("-------------------....REUSE SOCKET....-------------------")
             else:
                 s = socket.socket(
@@ -119,14 +150,15 @@ class URL:
                     s = ctx.wrap_socket(s, server_hostname=self.host)
                 # add new socket and response to reuse them in future
                 response = s.makefile("rb")
-                SOCKETS[key] = s, response
+                SOCKETS[socket_key] = s, response
                 print("-------------------...OPEN NEW SOCKET...-------------------")
 
             # Request Manipulation
             headers = {
                 "Host": self.host,
                 "Connection": "keep-alive",
-                "User-Agent": "KerimBrowser"
+                "User-Agent": "KerimBrowser",
+                "Accept-Encoding": "gzip"
             }
             # Start with the Method Path Version
             request = "GET {} HTTP/1.1\r\n".format(self.path)
@@ -140,6 +172,7 @@ class URL:
             statusline = response.readline().decode("utf-8")
             version, status, explanation = statusline.split(" ", 2)
 
+
             response_headers = {}
             while True:
                 line = response.readline()
@@ -149,12 +182,43 @@ class URL:
                 header, value = line.split(":", 1)
                 response_headers[header.casefold()] = value.strip()
 
-            assert "transfer-encoding" not in response_headers
-            assert "content-encoding" not in response_headers
+            print(response_headers) # just to check response headers what they include
+            if "transfer-encoding" in response_headers:
+                assert response_headers["transfer-encoding"] == "chunked"
+            if "content-encoding" in response_headers:
+                assert response_headers["content-encoding"] == "gzip"
 
-            content_len = int(response_headers["content-length"])
-            content = response.read(content_len).decode("utf-8")
+            if response_headers.get("transfer-encoding") == "chunked":
+                content = b""
 
+                while True:
+                    size_line = response.readline()
+                    size = int(size_line, 16)
+
+                    if size == 0:
+                        response.readline()
+                        break
+
+                    content += response.read(size)
+                    response.readline()
+            else:
+                content_len = int(response_headers["content-length"])
+                content = response.read(content_len)
+
+            if response_headers.get("content-encoding") == "gzip":
+                content = gzip.decompress(content)
+            
+            content = content.decode("utf-8")
+
+            cache_control = response_headers.get("cache-control")
+            
+            if status == "200":
+                if cache_control and "no-store" in cache_control:
+                    pass
+                elif cache_control and "max-age=" in cache_control:
+                    print("SAVED TO CACHE", cache_key, cache_control)   
+                    CACHE[cache_key] = time.time(), status, response_headers, content
+        
         return status, response_headers, content
     
 if __name__ == "__main__":
@@ -163,5 +227,5 @@ if __name__ == "__main__":
         load(URL(sys.argv[1]))
     else:
         # load(URL(DEFAULT_URL))
-        load(URL("http://browser.engineering/"))
-        load(URL("http://browser.engineering/http.html"))
+        # load(URL("http://browser.engineering/http.html"))
+        load(URL("http://browser.engineering/http.html/http.html"))
